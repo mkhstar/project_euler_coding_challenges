ea6bc9237647173ce224780a8935b44de75273fb
Kusi Musah Hussein <kusimusah@gmail.com>
2018-09-01 15:58:26 +0300

    Solve Problem 6

--- main.js
+++ main.js
@@ -117,3 +117,34 @@ function evenlyDivisible() {
   }
   console.log(number);
 }
+
+////////////////////
+// Problem 6
+/*
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+function difference() {
+  let squaredSum = 0;
+  let sumSquared = 0;
+  let difference = 0;
+
+  for (let i = 1; i <= 100; i++) {
+    squaredSum += i * i;
+  }
+  for (let i = 1; i <= 100; i++) {
+    sumSquared += i;
+  }
+  sumSquared *= sumSquared;
+  difference = sumSquared - squaredSum;
+  console.log(difference);
+}
+difference();
