a2b9e4f7d8f33ee88bd11a37f27a458d2b5d5ca4
Kusi Musah Hussein <kusimusah@gmail.com>
2018-09-01 15:08:04 +0300

    Solve problem 5 on Project euler

--- main.js
+++ main.js
@@ -96,3 +96,24 @@ function largestPalProduct() {
   }
   console.log(largestProduct, arrayValues);
 }
+
+///////////////////////
+// Problem 5
+/*
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+
+function evenlyDivisible() {
+  let numberFound = false;
+  let number = 1;
+  while (!numberFound) {
+    numberFound = true;
+    for (let i = 1; i <= 20; i++) {
+      if (number % i !== 0)
+        numberFound = false;
+    }
+    numberFound ? number : number++;
+  }
+  console.log(number);
+}
